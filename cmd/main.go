01288374e371b81572b102471ff31b7cedb4a8ae
qinjinze <18603034621@qq.com>
2024-05-07 16:03:29 +0800

    优化日志写入数据库和从数据库读取日志

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"github.com/qinjinze/alog"
-
+	//"alog"
 	"time"
 )
 
 // 测试自己写的日志库
 func main() {
-	// 日志配置，生存文件或写入数据库，并且实时跟踪日志才需要配置
-	//log := alog.LogConfig{
-	//	IsConsole:    true,
-	//	Level:        "debug",
-	//	Color:        true,
-	//	IsFile:       true,
-	//	LogName:      "log.log",
-	//	LogPath:      "./",
-	//	IsError:      false,
-	//	ErrorLogPath: "errorlog.log",
-	//	ErrorLogName: "./errorlog.log",
-	//	SaveDbType:   "mysql",
-	//}
-	//
-	//log.InitLogConfig()
+	// 初始化日志配置,可以不先填写，使用默认配置
+	log := alog.LogConfig{
+		IsConsole:    true,
+		Level:        "debug",
+		Color:        true,
+		IsFile:       true,
+		LogName:      "log.log",
+		LogPath:      "./",
+		IsError:      false,
+		ErrorLogPath: "errorlog.log",
+		ErrorLogName: "./errorlog.log",
+		SaveDbType:   "mysql",
+		LogType:      "platform", //user,platform、device
+		UserName:     "",         //用户名
+		RequestId:    "",         //每次API请求生成唯一ID
+		Page:         "",         //页面
+		Api:          "",         //api名称
+		Function:     "",         //方法函数
+		Seller:       "",
+		SellerId:     "",
+		Token:        "",
+		DbHost:       "127.0.0.1",
+		DbPort:       "3306",
+		DbUserName:   "",
+		DbPassword:   "",
+		DbName:       "",
+	}
+
+	log.InitLogConfig()
 	id := 0
 	//name := "loge"
 	//alog.TimeFormat = "2006-01-02 15:04:05.999999999 -0700 MST"
@@ -37,20 +50,20 @@ func main() {
 		//alog.Warn("这是一条Warning日志%s", name)
 		//alog.Error("这是一条Error日志", id, name)
 		//alog.Fatal("这是一条Fatal日志", name)
-		alog.Unknown("亮色字体", 1, time.Now())
-		fmt.Println()
-		alog.Debug("白色字体", 2, time.Now())
-		fmt.Println()
-		alog.Info("蓝色字体", 3, time.Now())
-		fmt.Println()
-		alog.Warn("黄色字体", 4, time.Now())
-		fmt.Println()
-		alog.Error("红色字体", 5, time.Now())
-		fmt.Println()
-		alog.Fatal("粉色字体", 6, time.Now())
-		fmt.Println()
-		alog.Invade("绿色字体", 6, time.Now())
-		fmt.Println()
+		//alog.Unknown("亮色字体", 1, time.Now())
+		//fmt.Println()
+		//alog.Debug("白色字体", 2, time.Now())
+		//fmt.Println()
+		//alog.Info("蓝色字体", 3, time.Now())
+		//fmt.Println()
+		//alog.Warn("黄色字体", 4, time.Now())
+		//fmt.Println()
+		//alog.Error("红色字体", 5, time.Now())
+		//fmt.Println()
+		//alog.Fatal("粉色字体", 6, time.Now())
+		//fmt.Println()
+		//alog.Invade("绿色字体", 6, time.Now())
+		//fmt.Println()
 		//alog.Uf("亮色字体%d, %s", 1, time.Now())
 		//alog.Df("白色字体%d, %s", 2, time.Now())
 		//alog.If("蓝色字体%d, %s", 3, time.Now())
@@ -61,9 +74,14 @@ func main() {
 		//content, err := Hello()
 		//alog.If("格式化打印id=%d,name=%s", content, err)
 		//logger.Info("测试日志库", id, name)
-
 		//log.Info("写入文件中", id, time.Now())
-		//log.Uf("亮色字体%d, %s", 1, time.Now())
+		log.UserName = "admin"
+		log.RequestId = "123456"
+		log.Page = "index"
+		log.Api = "/login"
+		log.Function = "login"
+		log.LogType = "platform"
+		log.Uf("亮色字体%d, %s", id, time.Now())
 		//log.Df("白色字体%d, %s", 2, time.Now())
 		//log.If("蓝色字体%d, %s", 3, time.Now())
 		//log.Wf("黄色字体%d, %s", 4, time.Now())
@@ -87,7 +105,7 @@ func main() {
 		//log.Info("写入文件中", id, name)
 		//time.Sleep(1 * time.Second)
 		//break
-		time.Sleep(1 * time.Second)
+		//time.Sleep(3 * time.Second)
 
 	}
 	//logger.Info("测试日志库")
--- model/model.go
+++ model/model.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/beego/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	//"github.com/qinjinze/alog/utils"
 	"alog/utils"
+	//_ "github.com/qinjinze/alog/utils"
 	_ "alog/utils"
 	"github.com/wonderivan/logger"
 	"gorm.io/driver/mysql"
@@ -12,18 +14,21 @@ import (
 	"time"
 )
 
-// 设备日志
+// 实时监控的历史轨迹日志
 type DeviceLog struct {
 	Id int64 `json:"DeviceLogId" gorm:"primaryKey"` //Id
-	Level      string    `orm:"size(10);NULL;description(日志等级名称)" json:"Level" gorm:"comment:日志等级名称"`           //
-	LevelInt   int       `orm:"size(2);NULL;description(日志等级)" json:"LevelInt" gorm:"comment:日志等级"`             //
-	Function   string    `orm:"size(100);NULL;description(功能模块)" json:"Function" gorm:"comment:功能模块"`           //
-	RequestId  string    `orm:"size(30);NULL;description(每次请求的唯一id)" json:"RequestId" gorm:"comment:每次请求的唯一id"` //
+	//Sn         string    `orm:"size(30);description(编号)" json:"Sn"`                    //
+	Level      string    `orm:"size(10);NULL;description(日志等级名称)" json:"Level" gorm:"comment:日志等级名称"`             //
+	LevelInt   int       `orm:"size(2);NULL;description(日志等级)" json:"LevelInt" gorm:"comment:日志等级"`               //
+	Function   string    `orm:"size(100);NULL;description(功能模块)" json:"Function" gorm:"comment:功能模块"`             //
+	RequestId  string    `orm:"size(30);NULL;description(每次请求的唯一id)" json:"RequestId" gorm:"comment:每次请求的唯一id"`   //
 	Token      string    `orm:"size(255);NULL;description(设备登录Token)" json:"Token" gorm:"comment:设备登录Token"`      //
-	UserName   string    `orm:"size(255);NULL;description(设备唯一id或登录名)" json:"UserName" gorm:"comment:设备唯一id或登录名"`         //
+	UserName   string    `orm:"size(255);NULL;description(设备唯一id或登录名)" json:"UserName" gorm:"comment:设备唯一id或登录名"` //
 	Content    string    `orm:"size(1024);NULL;description(日志内容)" json:"Content" gorm:"comment:日志内容;type:text"`   //
-	LogTime    time.Time `orm:"size(15);NULL;description(日志生成时间)" json:"LogTime" gorm:"comment:日志生成时间"`
+	LogTime    time.Time `orm:"size(6);NULL;description(日志生成时间)" json:"LogTime" gorm:"comment:日志生成时间"`
 	CreateTime time.Time `orm:"size(6);auto_now_add;type(datetime);description(插入表中时间)" json:"CreateTime" gorm:"autoCreateTime;comment:插入表中时间"`
+	Seller     string    `orm:"size(255);NULL;description(商家简称：网店名称)" json:"Seller" gorm:"comment:商家简称：网店名称"` //
+	SellerId   string    `orm:"size(100);NULL;description(商家编号)" json:"SellerId" gorm:"comment:商家编号"`         //
 }
 
 // 管理后台日志
@@ -33,11 +38,14 @@ type PlatformLog struct {
 	LevelInt   int       `orm:"size(2);NULL;description(日志等级)" json:"LevelInt" gorm:"comment:日志等级"`             //
 	Function   string    `orm:"size(100);NULL;description(功能模块)" json:"Function" gorm:"comment:功能模块"`           //
 	RequestId  string    `orm:"size(30);NULL;description(每次请求的唯一id)" json:"RequestId" gorm:"comment:每次请求的唯一id"` //
-	Token      string    `orm:"size(255);NULL;description(用户登录Token)" json:"Token" gorm:"comment:用户登录Token"`      //
+	Token      string    `orm:"size(255);NULL;description(用户登录Token)" json:"Token" gorm:"comment:用户登录Token"`    //
 	UserName   string    `orm:"size(255);NULL;description(账户)" json:"UserName" gorm:"comment:用户登录唯一id"`         //
-	Content    string    `orm:"size(1024);NULL;description(日志内容)" json:"Content" gorm:"comment:日志内容;type:text"`   //
-	LogTime    time.Time `orm:"size(15);NULL;description(日志生成时间)" json:"LogTime" gorm:"comment:日志生成时间"`
+	Content    string    `orm:"size(1024);NULL;description(日志内容)" json:"Content" gorm:"comment:日志内容;type:text"` //
+	LogTime    time.Time `orm:"size(6);NULL;description(日志生成时间)" json:"LogTime" gorm:"comment:日志生成时间"`
 	CreateTime time.Time `orm:"size(6);auto_now_add;type(datetime);description(插入表中时间)" json:"CreateTime" gorm:"autoCreateTime;comment:插入表中时间"`
+	Seller     string    `orm:"size(255);NULL;description(商家简称：网店名称)" json:"Seller" gorm:"comment:商家简称：网店名称"` //
+	SellerId   string    `orm:"size(100);NULL;description(商家编号)" json:"SellerId" gorm:"comment:商家编号"`         //
+
 }
 
 // C端用户日志
@@ -47,11 +55,14 @@ type UserLog struct {
 	LevelInt   int       `orm:"size(2);NULL;description(日志等级)" json:"LevelInt" gorm:"comment:日志等级"`             //
 	Function   string    `orm:"size(100);NULL;description(功能模块)" json:"Function" gorm:"comment:功能模块"`           //
 	RequestId  string    `orm:"size(30);NULL;description(每次请求的唯一id)" json:"RequestId" gorm:"comment:每次请求的唯一id"` //
-	Token      string    `orm:"size(255);NULL;description(用户登录Token)" json:"Token" gorm:"comment:用户登录Token"`      //
+	Token      string    `orm:"size(255);NULL;description(用户登录Token)" json:"Token" gorm:"comment:用户登录Token"`    //
 	UserName   string    `orm:"size(255);NULL;description(账户)" json:"UserName" gorm:"comment:用户登录唯一id"`         //
-	Content    string    `orm:"size(1024);NULL;description(日志内容)" json:"Content" gorm:"comment:日志内容;type:text"`   //
-	LogTime    time.Time `orm:"size(15);NULL;description(日志生成时间)" json:"LogTime" gorm:"comment:日志生成时间"`
+	Content    string    `orm:"size(1024);NULL;description(日志内容)" json:"Content" gorm:"comment:日志内容;type:text"` //
+	LogTime    time.Time `orm:"size(6);NULL;description(日志生成时间)" json:"LogTime" gorm:"comment:日志生成时间"`
 	CreateTime time.Time `orm:"size(6);auto_now_add;type(datetime);description(插入表中时间)" json:"CreateTime" gorm:"autoCreateTime;comment:插入表中时间"`
+	Seller     string    `orm:"size(255);NULL;description(商家简称：网店名称)" json:"Seller" gorm:"comment:商家简称：网店名称"` //
+	SellerId   string    `orm:"size(100);NULL;description(商家编号)" json:"SellerId" gorm:"comment:商家编号"`         //
+
 }
 
 // 用户信息
@@ -60,6 +71,7 @@ type User struct {
 	UserName string `orm:"size(255);NULL;description(账户)" json:"UserName" gorm:"comment:用户登录唯一id"` //
 	Password string `orm:"size(255);NULL;description(密码)" json:"Password" gorm:"comment:用户登录密码"`   //
 }
+
 var Db *gorm.DB
 var err error
 
@@ -118,8 +130,8 @@ func InitModel(dbUserName, dbPassword, dbAddr, dbPort, dbName string) {
 		panic("数据库连接失败，err=" + err.Error())
 		return
 	}
-	sqlDB.SetMaxIdleConns(30)    //设置连接池，空闲
-	sqlDB.SetMaxOpenConns(10000) //设置打开最大连接
+	sqlDB.SetMaxIdleConns(30)  //设置连接池，空闲
+	sqlDB.SetMaxOpenConns(100) //设置打开最大连接
 
 	// 迁移 schema ，自动创建表结构
 	err = Db.AutoMigrate(&DeviceLog{}, &PlatformLog{}, &UserLog{}, &User{})
@@ -144,25 +156,25 @@ func openMysql(dsn string) {
 	}
 }
 
-func openSqlite(dsn string) {
-	Db, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
-}
-func openPostgres(dsn string) {
-	Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-}
-func openOrcle(dsn string) {
-
-}
-func openTidb(dsn string) {
-
-}
-func openEs(dsn string) {
-
-}
-func openClickHouse(dsn string) {
-	Db, err = gorm.Open(clickhouse.Open(dsn), &gorm.Config{})
-}
-
-func openSqlserver(dsn string) {
-	Db, err = gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
-}
+//func openSqlite(dsn string) {
+//	Db, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+//}
+//func openPostgres(dsn string) {
+//	Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+//}
+//func openOrcle(dsn string) {
+//
+//}
+//func openTidb(dsn string) {
+//
+//}
+//func openEs(dsn string) {
+//
+//}
+//func openClickHouse(dsn string) {
+//	Db, err = gorm.Open(clickhouse.Open(dsn), &gorm.Config{})
+//}
+//
+//func openSqlserver(dsn string) {
+//	Db, err = gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+//}
